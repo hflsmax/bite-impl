95fdc0f44c9fcedef00ca711be58a463a453164c
Cong Ma <cong.ma@uwaterloo.ca>
2023-03-24 11:06:20 -0400

    setup nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,64 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    # Convenience functions for writing flakes
+    flake-utils.url = "github:numtide/flake-utils";
+    # Precisely filter files copied to the nix store
+    nix-filter.url = "github:numtide/nix-filter";
   };
+
+  outputs = { self, nixpkgs, flake-utils, nix-filter }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      legacyPackages = nixpkgs.legacyPackages.${system};
+      ocamlPackages = legacyPackages.ocamlPackages;
+
+      # Filtered sources (prevents unecessary rebuilds)
+      sources = {
+        ocaml = nix-filter.lib {
+          root = ./.;
+          include = [
+            ".ocamlformat"
+            "dune-project"
+            (nix-filter.lib.inDirectory "bin")
+            (nix-filter.lib.inDirectory "lib")
+            (nix-filter.lib.inDirectory "test")
+          ];
+        };
+
+        nix = nix-filter.lib {
+          root = ./.;
+          include = [
+            (nix-filter.lib.matchExt "nix")
+          ];
+        };
+      };
+    in {
+      packages = {
+        # The package that will be built or run by default. For example:
+        #
+        #     $ nix build
+        #     $ nix run -- <args?>
+        #
+        default = self.packages.${system}.hello;
+
+        hello = ocamlPackages.buildDunePackage {
+          pname = "bite";
+          version = "0.1.0";
+          duneVersion = "3";
+          src = sources.ocaml;
+
+          buildInputs = [ 
+            ocamlPackages.ppx_sexp_conv
+            ocamlPackages.core
+          ];
+
+          nativeBuildInputs = [
+            ocamlPackages.menhir
+          ];
+
+        };
+      };
+    });
 }
